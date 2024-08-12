c4df296a4c6deef5978376fc9ffc281348731288
coskunkus <coskunkus@gmail.com>
2024-08-12 23:20:41 +0300

    new3

--- R/pcdata.R
+++ R/pcdata.R
@@ -15,32 +15,18 @@
 #' Springer Science & Business Media.
 #' @examples
 #' pcdata(10,rep(1,10),"qweibull",c(2,3))
-pcdata=function(m,r,qdistr="qunif",par=c(0,1))
+pcdata<-function(m,r,qdistr="qunif",par=c(0,1))
 {
   if(any(m!=length(r))) {stop("dimension of r must be equal to m")}
-  x=NULL
-
-  y=NULL
-
-  n=m+sum(r)
-
-  x[1]=stats::rexp(1)/n
-
+  x<-NULL
+  y<-NULL
+  n<-m+sum(r)
+  x[1]<-stats::rexp(1)/n
   for(i1 in 2:m){
-
-    x[i1]=x[i1-1]+stats::rexp(1)/(n-sum(r[1:(i1-1)])-i1+1)
-
+    x[i1]<-x[i1-1]+stats::rexp(1)/(n-sum(r[1:(i1-1)])-i1+1)
   }
-
-  u=1-exp(-x)
-
-  f=paste0("y=",qdistr,"(u,par)")
-
+  u<-1-exp(-x)
+  f<-paste0("y<-",qdistr,"(u,par)")
   eval(parse(text = f))
-
-
   return(y)
 }
-devtools::check()
-rhub::check_for_cran()
-devtools::check_win_devel()
--- tests/testthat/test_function.R
+++ tests/testthat/test_function.R
@@ -5,10 +5,12 @@ test_that("functions returns a list", {
 })
 # ## Test whether the code throw an error.
 test_that("functions returns errors", {
-  expect_error(pcdata(9,rep(1,10),"qweibull",c(1,2)), "dimension of r must be equal to m")
+  expect_error(pcdata(9,rep(1,10),"qweibull",c(1,2)),
+               "dimension of r must be equal to m")
 })
 # ## Test whether the output is a vector with the expected size
 test_that("functions returns a  vector with the expected size", {
-  expect_vector(pcdata(10,rep(1,10),"qweibull",c(1,2)), ptype = double(), size = 10)
+  expect_vector(pcdata(10,rep(1,10),"qweibull",c(1,2)),
+                ptype = double(), size = 10)
 })
 
